ab1b075bdefa3ddd218c6b19565018bb8959e547
Morten Nygaard Åsnes <morten.aasnes@gmail.com>
2026-02-18 14:50:43 +0100

    docs: configure base for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/p5js-workshop/',
   vite: {
     plugins: [tailwindcss()]
   },
